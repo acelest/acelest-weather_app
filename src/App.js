fb938b7f576fa0ff7486296d9ac1cdd2801d6f43
Aubin <eyengadjou@gmail.com>
2023-11-03 18:45:21 +0100

    structure jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+// import axios from "axios";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  // const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Douala</p>
+          </div>
+          <div className="temp"></div>
+          <h1>30 Celsus</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>60 * F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
